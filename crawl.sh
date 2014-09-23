511556fd80622fdb2562395bddb8f69510ede592
Runar Myklebust <rmy@enonic.com>
2014-09-23 11:47:27 +0200

    renamed executables

---
+++ crawl.sh
@@ -0,0 +1,8 @@
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || die "configuration missing, e.g crawl.sh ./configurations/cms-46ce-unstable-enonic_vtnode2_8080_crawler.properties "
+
+java -jar ./lib/mightycrawler-jar-with-dependencies.jar $1
--- run.sh
+++
@@ -1,8 +0,0 @@
-die () {
-    echo >&2 "$@"
-    exit 1
-}
-
-[ "$#" -eq 1 ] || die "configuration missing, e.g run.sh ./configurations/cms-46ce-unstable-enonic_vtnode2_8080_crawler.properties "
-
-java -jar ./lib/mightycrawler-jar-with-dependencies.jar $1
